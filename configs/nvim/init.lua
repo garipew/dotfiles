eb4fa760a7100bb8994cdd7a809c3b7790532900
Gabriel Sá <gabriel10043@live.com>
2025-10-12 23:15:17 -0300

    add minimal setup in init.lua

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -2,7 +2,7 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.g.start_time = vim.fn.reltime()
-vim.loader.enable()
+-- vim.loader.enable()
 
 vim.call('plug#begin')
 Plug('uZer/pywal16.nvim')
@@ -10,3 +10,30 @@ vim.call('plug#end')
 
 local pywal16 = require('pywal16')
 pywal16.setup()
+
+vim.cmd("syntax enable")
+
+-- Relative numbers for crazy jumps!!!
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- incremental search ftw
+vim.opt.incsearch = true
+
+-- Center cursor after half-page jump
+vim.keymap.set('n', '<C-d>', '<C-d>zz', {noremap=true, silent=true})
+vim.keymap.set('n', '<C-u>', '<C-u>zz', {noremap=true, silent=true})
+
+-- Buffer stuff, may be useful later
+vim.keymap.set('n', '<M-l>', ':bn<CR>', {noremap=true, silent=true})
+vim.keymap.set('n', '<M-h>', ':bp<CR>', {noremap=true, silent=true})
+
+-- Man page on tmux pane!!!
+vim.keymap.set('n', 'K', function()
+	local word = vim.fn.expand('<cword>')
+	if os.getenv("TMUX") ~= nil then
+		vim.fn.system("tmux split-window -h 'man " .. word .. "'")
+	else
+		vim.cmd("!man " .. word)	
+	end
+end)
